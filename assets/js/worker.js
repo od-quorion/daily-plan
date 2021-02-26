611a19deae22f5afd35c1db186a66df68a0c35f0
osman-durdag <o.durdag@quorion-qbs.com>
2021-02-26 17:24:01 +0300

    .

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -54,49 +54,54 @@ self.onmessage = msg => {
     // console.log(msg.data);
     // console.log(msg);
 
-    msg.data.forEach((job, index) =>{
-        if(localStorage.getItem(`${index + 1}.job`) === "notShowed"){
-            if(job.beginTime.getTime() <= date.getTime() && job.endTime.getTime() > date.getTime()){
-                const notification = new Notification('JavaScript Notification API', {
-                    body: `${job.name} Time`,
-                    icon: '../img/quorion_logo.svg'
-                });
-
-                setTimeout(() => {
-                    notification.close();
-                }, 10 * 1000);
-
-                notification.addEventListener('click', () => {
-                    window.open('https://od-quorion.github.io/daily-plan', '_blank');
-                });
-
-                localStorage.setItem(`${index + 1}.job`, "showed");
-            }
-        }
+    const notification = new Notification('JavaScript Notification API', {
+        body: `${job.name} Time`,
+        icon: '../img/quorion_logo.svg'
     });
 
-    setInterval(() => {
-        msg.data.forEach((job, index) =>{
-            if(localStorage.getItem(`${index + 1}.job`) === "notShowed"){
-                if(job.beginTime.getTime() <= date.getTime() && job.endTime.getTime() > date.getTime()){
-                    const notification = new Notification('JavaScript Notification API', {
-                        body: `${job.name} Time`,
-                        icon: '../img/quorion_logo.svg'
-                    });
+    // msg.data.forEach((job, index) =>{
+    //     // if(localStorage.getItem(`${index + 1}.job`) === "notShowed"){
+    //     //     if(job.beginTime.getTime() <= date.getTime() && job.endTime.getTime() > date.getTime()){
+    //     //         const notification = new Notification('JavaScript Notification API', {
+    //     //             body: `${job.name} Time`,
+    //     //             icon: '../img/quorion_logo.svg'
+    //     //         });
+
+    //     //         setTimeout(() => {
+    //     //             notification.close();
+    //     //         }, 10 * 1000);
+
+    //     //         notification.addEventListener('click', () => {
+    //     //             window.open('https://od-quorion.github.io/daily-plan', '_blank');
+    //     //         });
+
+    //     //         localStorage.setItem(`${index + 1}.job`, "showed");
+    //     //     }
+    //     // }
+    // });
+
+    // setInterval(() => {
+    //     msg.data.forEach((job, index) =>{
+    //         if(localStorage.getItem(`${index + 1}.job`) === "notShowed"){
+    //             if(job.beginTime.getTime() <= date.getTime() && job.endTime.getTime() > date.getTime()){
+    //                 const notification = new Notification('JavaScript Notification API', {
+    //                     body: `${job.name} Time`,
+    //                     icon: '../img/quorion_logo.svg'
+    //                 });
     
-                    setTimeout(() => {
-                        notification.close();
-                    }, 10 * 1000);
+    //                 setTimeout(() => {
+    //                     notification.close();
+    //                 }, 10 * 1000);
     
-                    notification.addEventListener('click', () => {
-                        window.open('https://od-quorion.github.io/daily-plan', '_blank');
-                    });
+    //                 notification.addEventListener('click', () => {
+    //                     window.open('https://od-quorion.github.io/daily-plan', '_blank');
+    //                 });
     
-                    localStorage.setItem(`${index + 1}.job`, "showed");
-                }
-            }
-        });
+    //                 localStorage.setItem(`${index + 1}.job`, "showed");
+    //             }
+    //         }
+    //     });
         
-    }, 300000);
+    // }, 300000);
 };
 
