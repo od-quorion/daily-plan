228144662770cba1d5086caa1ffced031e1b2334
osman-durdag <o.durdag@quorion-qbs.com>
2021-02-26 14:49:30 +0300

    .

--- assets/js/worker.js
+++ assets/js/worker.js
@@ -1,3 +1,34 @@
+let tableRows = document.querySelectorAll("tbody tr");
+
+let allJobs = [];
+
+tableRows.forEach((row, index) => {
+
+    let beginTime = row.children[1].innerHTML.split(":");
+    let beginTimeHour = parseInt(beginTime[0]);
+    let beginTimeMinute = parseInt(beginTime[1]);
+   
+    beginTime = new Date(year, month - 1, day, beginTimeHour, beginTimeMinute);
+
+    let endTime = row.children[2].innerHTML.split(":");
+    let endTimeHour = parseInt(endTime[0]);
+    let endTimeMinute = parseInt(endTime[1]);
+    
+    endTime = new Date(year, month - 1, day, endTimeHour, endTimeMinute);
+    
+    allJobs.push({
+        name : row.children[0].innerHTML,
+        beginTime,
+        endTime,
+        beginTimeHour,
+        beginTimeMinute,
+        endTimeHour,
+        endTimeMinute,
+        isShowed : false
+    });
+    
+});
+
 allJobs.forEach((job, index) =>{
     if(localStorage.getItem(`${index + 1}.job`) === "notShowed"){
         if(job.beginTime.getTime() <= date.getTime() && job.endTime.getTime() > date.getTime()){
