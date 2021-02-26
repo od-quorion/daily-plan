c7cf89eea98f69f3921e04030e5667ab57bec20c
osman-durdag <o.durdag@quorion-qbs.com>
2021-02-26 12:03:28 +0300

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -124,20 +124,22 @@ if (Notification.permission === 'granted') {
     setInterval(() => {
         allJobs.forEach((job, index) =>{
             if(localStorage.getItem(`${index + 1}.job`) === "notShowed"){
-                const notification = new Notification('JavaScript Notification API', {
-                    body: `${job.name} Time`,
-                    icon: '../img/quorion_logo.svg'
-                });
-
-                setTimeout(() => {
-                    notification.close();
-                }, 10 * 1000);
-
-                notification.addEventListener('click', () => {
-                    window.open('https://od-quorion.github.io/daily-plan', '_blank');
-                });
-
-                localStorage.setItem(`${index + 1}.job`, "showed");
+                if(job.beginTime.getTime() <= date.getTime() && job.endTime.getTime() > date.getTime()){
+                    const notification = new Notification('JavaScript Notification API', {
+                        body: `${job.name} Time`,
+                        // icon: '../img/quorion_logo.svg'
+                    });
+    
+                    setTimeout(() => {
+                        notification.close();
+                    }, 10 * 1000);
+    
+                    notification.addEventListener('click', () => {
+                        window.open('https://od-quorion.github.io/daily-plan', '_blank');
+                    });
+    
+                    localStorage.setItem(`${index + 1}.job`, "showed");
+                }
             }
         });
         
